4a4a1c9edb81a02481b43a323c05dae36e794e5c
vevis <vevisvs@gmail.com>
2023-04-25 20:14:20 -0300

    Cambio de iconos en card de servicios

--- veterinary-clinic/src/components/Services/ServiceCard.jsx
+++ veterinary-clinic/src/components/Services/ServiceCard.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import data from './ServiceData.json'
+import {FaAmbulance} from 'react-icons/fa'
+import{GiRadioactive} from 'react-icons/gi'
+import {TbCircuitResistor} from 'react-icons/tb'
+import {BiInjection} from 'react-icons/bi'
+import {MdLocalHospital} from 'react-icons/md'
+import {MdOutlinePets} from 'react-icons/md'
 
 const ServiceCard = () => {
+  const iconMap = {
+    GiRadioactive,
+    FaAmbulance,
+    TbCircuitResistor,
+    BiInjection,
+    MdLocalHospital,
+    MdOutlinePets
+  };
+
   return (
     <div className='service-card-info'>
       {
         data.map(item => (
           <div className='card'>
-            <div className='card-icon'>{item.icon}</div>
+            {/* <div className='card-icon'>{item.icon}</div> */}
+            {React.createElement(iconMap[item.icon], { className: 'card-icon' })}
             <h2 className='card-title'>{item.title}</h2>
             <p className='card-description'>{item.description}</p>
           </div>
