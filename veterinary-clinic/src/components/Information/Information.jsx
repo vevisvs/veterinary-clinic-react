3c19ae74431cd7d6886fdaab43e5f13236c7f62d
vevis <vevisvs@gmail.com>
2023-07-11 01:09:38 -0300

    el usuario puede acceder al template de reserva solo si antes ha iniciado sesión, de lo contrario el boton no figura en la pantalla

--- veterinary-clinic/src/components/Information/Information.jsx
+++ veterinary-clinic/src/components/Information/Information.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import '../Information/Information.css'
 import photo1 from '../../assets/info-vet.jpg'
+import { Link } from "react-router-dom";
+import { UserContext } from '../../context/UserContext';
 
 const Information = () => {
+  const { userLogged } = useContext(UserContext);
+ 
   return (
     <div className='ctn-information'>
         <img className='info-vet-image' src={photo1}/>
         <div className='info-description'>
             <h2>Los mejores médicos veterinarios de todas las especialidades</h2>
             <p className='text-parrafo'>Todos nuestros veterinarios son expertos en su campo y graduados de las mejores universidades. Nuestro equipo de especialistas trabaja para brindarte el servicio que necesites.</p>
-            <button className='info-button'>Contáctanos</button>
+              {
+                userLogged.name &&
+                <button className='info-button'><Link to="/booking">Reservar una cita</Link></button>
+              }
         </div>
     </div>
   )
